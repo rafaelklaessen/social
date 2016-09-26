54420b14a87c8599506a19ec5ed696437073e199
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2016-09-26 20:08:17 +0100

    Update

--- brunch-config.js
+++ brunch-config.js
@@ -22,6 +22,7 @@ exports.config = {
     stylesheets: {
       joinTo: "css/app.css",
       order: {
+        after: ["web/static/css/app.scss"] // concat app.scss last
       }
     },
     templates: {
