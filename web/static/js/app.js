36ca9fcd8a59fe3b7c2a1065ca56c1d812b300c3
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2016-10-11 13:53:51 +0100

    Front-end app.html layout

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,50 +1,77 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+import $ from "jquery"
 
 /**
- * Spooky scary ES6
- * (demo)
+ * TODO: Implement OOP when stuff is done 
  */
-class Kees {
-  static kaas() {
-    return true;
-  }
+
+
+// Close modals
+$('.modal .title-container .close-btn').click(function() {
+  var modal = $(this).parents('.modal');
+  closeModal(modal);
+});
+/**
+ * Open modal function
+ * Opens given modal
+ * @param {string/element} modal to open
+ */
+function openModal(modal) {
+  $(modal).parents('.overlay').fadeIn(400);
+  $(modal).addClass('open');
 }
-let k = z => `jan ${z}`,
-    l = () => 'piet';
-const p = (y = 'kaas') => `text: ${y}`
-console.log(k);
-console.log(k('kees'));
-console.log(l);
-console.log(l());
-console.log(p);
-console.log(p());
-console.log(p('kees'));
-console.log(Kees.kaas());
-let logAll = (toLog) => {
-  let arr = toLog.split('');
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
+/**
+ * Close modal function
+ * Closes given modal
+ * @param {string/element} modal to close
+ */
+function closeModal(modal) {
+  $(modal).parents('.overlay').fadeOut(400)
+  $(modal).removeClass('open');
 }
-console.log(logAll);
-logAll('I love ES6!');
+/**
+ * Notify function
+ * Shows given notification content in a notification
+ * @param {string} notification content
+ */
+function notify(notification) {
+  alert(notification);
+}
+// Open and close header dropdown
+$('#header .container #actions .actions-item-container .profile-actions .profile-pic').click(function() {
+  $(this).siblings('.profile-actions-dropdown').stop().toggle(200);
+});
+// Open Compose Tweet modal when Tweet button is clicked
+$('#global-new-tweet-btn').click(function() {
+  openModal('#new-tweet .modal');
+});
+// Close Compose Tweet modal when Tweet button is clicked
+// TODO: Add AJAX request to file that puts the Tweet in the database
+$('#new-tweet .modal .content-container .btn-container .right-container .tweet-btn').click(function() {
+  closeModal($(this).parents('.modal'));
+  setTimeout(function() {
+    notify('Your Tweet was posted!');
+  }, 400);
+});
+// Calculate remaining chars for Tweet
+$('#new-tweet .modal .content-container .compose-field').bind('keyup keydown click input propertychange', function() {
+  setTweetRemainingChars(this);
+});
+// Calculates and sets the remaining characters. When the number of remaining characters 
+// is smaller than 0, the Tweet button is disabled.
+function setTweetRemainingChars(el) {
+  var rightContainer = $(el).parents('.content-container').find('.right-container'), 
+      inputLength = $(el).val().length,
+      remainingChars = 140 - inputLength;
+  if (remainingChars < 0) {
+    rightContainer.find('.tweet-btn').attr('disabled', 'disabled');
+  } else {
+    rightContainer.find('.tweet-btn').removeAttr('disabled');
+  }
+  if (remainingChars <= 20) {
+    rightContainer.find('.char-count').addClass('warn');
+  } else {
+    rightContainer.find('.char-count').removeClass('warn');
+  }
+  rightContainer.find('.char-count').text(remainingChars);
+}
